9ccac90ec7216ef93dca89625a2a785a772a59b3
gabcytn <cayetanogabriel03@gmail.com>
2024-11-03 00:26:54 +0800

    Add repository annotation

--- toy-commerce-be/src/main/java/com/gabcytn/toycommerce/Repository/ItemRepository.java
+++ toy-commerce-be/src/main/java/com/gabcytn/toycommerce/Repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.gabcytn.toycommerce.Repository;
 
 import com.gabcytn.toycommerce.Model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 }
