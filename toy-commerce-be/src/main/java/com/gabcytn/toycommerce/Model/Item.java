6ee26d49b332d5164ae1c9aa7704eae6ab415d99
gabcytn <cayetanogabriel03@gmail.com>
2024-11-03 00:23:03 +0800

    Create item model without image

---
+++ toy-commerce-be/src/main/java/com/gabcytn/toycommerce/Model/Item.java
@@ -0,0 +1,65 @@
+package com.gabcytn.toycommerce.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Item {
+    @Id
+    private int id;
+    private String name;
+    private String description;
+    private int price;
+    private Date release_date;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return release_date;
+    }
+
+    public void setDate(Date date) {
+        this.release_date = date;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
