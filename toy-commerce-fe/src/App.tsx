500faab6223eb6516de656b8e4f4c6d4e2092b22
gabcytn <cayetanogabriel03@gmail.com>
2024-11-02 19:15:19 +0800

    Add main body tag

--- toy-commerce-fe/src/App.tsx
+++ toy-commerce-fe/src/App.tsx
@@ -1,6 +1,15 @@
 import Header from "./Header";
+import MainBody from "./MainBody";
+
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main className="bg-dark" style={{ minHeight: "calc(100dvh - 4.5rem)" }}>
+        <MainBody />
+      </main>
+    </>
+  );
 }
 
 export default App;
