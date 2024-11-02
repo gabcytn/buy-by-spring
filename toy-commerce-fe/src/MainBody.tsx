915ab6806a218cff575bff335a5910d0aa95c0e0
gabcytn <cayetanogabriel03@gmail.com>
2024-11-02 19:16:25 +0800

    Create container for cards

---
+++ toy-commerce-fe/src/MainBody.tsx
@@ -0,0 +1,18 @@
+import Card from "./Card";
+function MainBody() {
+  return (
+    <div className="container">
+      <div className="row g-3">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default MainBody;
