2aeb96a4dbf69f529a9e74e8abc03aa106232a11
gabcytn <cayetanogabriel03@gmail.com>
2024-11-02 19:52:27 +0800

    Initial commit

---
+++ toy-commerce-be/src/main/java/com/gabcytn/toycommerce/ToyCommerceBeApplication.java
@@ -0,0 +1,13 @@
+package com.gabcytn.toycommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyCommerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyCommerceBeApplication.class, args);
+	}
+
+}
---
+++ toy-commerce-be/src/test/java/com/gabcytn/toycommerce/ToyCommerceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabcytn.toycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyCommerceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
