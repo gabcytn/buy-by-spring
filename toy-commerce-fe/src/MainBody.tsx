329963f834f137a7baf321f2a02e78bba08f45e5
gabcytn <cayetanogabriel03@gmail.com>
2024-11-02 19:51:27 +0800

    Pass static ids for the meantime

--- toy-commerce-fe/src/MainBody.tsx
+++ toy-commerce-fe/src/MainBody.tsx
@@ -3,13 +3,13 @@ function MainBody() {
   return (
     <div className="container">
       <div className="row g-3">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card id={1} />
+        <Card id={2} />
+        <Card id={3} />
+        <Card id={4} />
+        <Card id={5} />
+        <Card id={6} />
+        <Card id={7} />
       </div>
     </div>
   );
