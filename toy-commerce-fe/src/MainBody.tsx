f6b5e7cb59e0b1914e797ec5331c1bbdda58c0b5
gabcytn <cayetanogabriel03@gmail.com>
2024-11-04 17:12:02 +0800

    Move props up for handling of search function

--- toy-commerce-fe/src/MainBody.tsx
+++ toy-commerce-fe/src/MainBody.tsx
@@ -1,23 +1,9 @@
-import { useState, useEffect } from "react";
 import Card from "./Card";
-function MainBody() {
-  const SERVER_URL = import.meta.env.VITE_SERVER_URL;
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      const res = await fetch(`${SERVER_URL}/items`);
-      const data = await res.json();
-      setItems(data);
-    };
-
-    fetchItems();
-  }, []);
-
+function MainBody({ items }: any) {
   return (
     <div className="container">
       <div className="row g-3">
-        {items.map((item, idx) => (
+        {items.map((item: Object, idx: number) => (
           <Card key={idx} render={item} />
         ))}
       </div>
