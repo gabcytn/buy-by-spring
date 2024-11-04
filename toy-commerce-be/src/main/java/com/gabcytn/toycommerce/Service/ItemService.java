c7deb2bc823708317647db57d4401827a794b9a9
gabcytn <cayetanogabriel03@gmail.com>
2024-11-04 12:08:31 +0800

    Break down functions and create update method

--- toy-commerce-be/src/main/java/com/gabcytn/toycommerce/Service/ItemService.java
+++ toy-commerce-be/src/main/java/com/gabcytn/toycommerce/Service/ItemService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.*;
 
 @Service
@@ -21,55 +22,94 @@ public class ItemService {
         this.cloudinary = cloudinary;
     }
 
-    public List<Item> getAllItems () {
-        return itemRepository.findAll();
+    public List<Item> getAllItems () { return itemRepository.findAll(); }
+
+    public Optional<Item> getItem (int id) { return itemRepository.findById(id); }
+
+    public void saveItem (Item item, MultipartFile image) {
+        checkContentTypeValidity(image.getContentType());
+        String[] imageProperties = uploadImageToCloudinary(image);
+        if (imageProperties.length > 0) {
+            item.setImage(imageProperties[0]);
+            item.setImagePublicID(imageProperties[1]);
+            item.setDate(LocalDate.now());
+            itemRepository.save(item);
+            return;
+        }
+
+        throw new RuntimeException("No image found");
     }
 
-    public Optional<Item> getItem (int id) {
-        return itemRepository.findById(id);
-    }
-
-    public void addItem (Item item) {
-        itemRepository.save(item);
-    }
-
-    public void deleteItem (int id) {
+    public void updateItem (Item item, MultipartFile image) {
         try {
-            if (itemRepository.findById(id).isPresent()) {
-                Item itemToBeDeleted = itemRepository.findById(id).get();
-                final String publicID = itemToBeDeleted.getImagePublicID();
-                cloudinary.uploader().destroy(publicID, null);
-                itemRepository.deleteById(id);
-            }
+            deleteImageInCloudinary(item.getId());
+            saveItem(item, image);
+        } catch (IOException e) {
+            System.err.println("Update error: " + e.getMessage());
+        }
+    }
+
+    public boolean deleteItem (int id) {
+        try {
+            deleteImageInCloudinary(id);
+            itemRepository.deleteById(id);
+            return true;
         }
         catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
+            return false;
         }
-
     }
 
-    public void addItemWithImage (Item item, MultipartFile image) {
-        final List<String> contentTypes = new ArrayList<>(Arrays.asList("image/jpeg", "image/png", "image"));
-        if (!contentTypes.contains(image.getContentType()))
-            throw new IllegalArgumentException();
+    public boolean itemExists (int id) {
+        return itemRepository.existsById(id);
+    }
 
-        final Map params1 = ObjectUtils.asMap(
+
+    // PRIVATE METHODS
+
+    private void deleteImageInCloudinary (int id) throws IOException {
+        Optional<Item> item = itemRepository.findById(id);
+        if (item.isPresent()) {
+            final String publicID = item.get().doGetImagePublicID();
+            cloudinary.uploader().destroy(publicID, null);
+        }
+    }
+
+    private String[] uploadImageToCloudinary (MultipartFile image) {
+        final Map uploadParams = ObjectUtils.asMap(
                 "use_filename", false,
                 "unique_filename", true,
                 "overwrite", true
         );
-
         try {
-            final Map uploadReturnValue = cloudinary.uploader().upload(image.getBytes(), params1);
+            final Map uploadReturnValue = cloudinary.uploader().upload(image.getBytes(), uploadParams);
             final String imageURL = uploadReturnValue.get("secure_url").toString();
             final String publicID = uploadReturnValue.get("public_id").toString();
-            item.setImage(imageURL);
-            item.setImagePublicID(publicID);
+            return new String[]{imageURL, publicID};
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
+            return new String[0];
+        }
+    }
+
+    private void checkContentTypeValidity (String contentType) {
+        if (!isImageContentTypeValid(contentType))
+            throw new IllegalArgumentException();
+    }
+
+    private boolean isImageContentTypeValid (String contentType) {
+        String[] contentTypes = { "image", "image/jpeg", "image/png", "image/gif", "image/webp" };
+        boolean isValid = false;
+
+        for (String type : contentTypes) {
+            if (type.equals(contentType)) {
+                isValid = true;
+                break;
+            }
         }
 
-        itemRepository.save(item);
+        return isValid;
     }
 }
 
