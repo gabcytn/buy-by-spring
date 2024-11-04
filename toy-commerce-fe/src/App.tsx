22380d2108573fbca4a5bb38a1742bb004e3b41f
gabcytn <cayetanogabriel03@gmail.com>
2024-11-04 17:08:30 +0800

    Handle all fetching items including search

--- toy-commerce-fe/src/App.tsx
+++ toy-commerce-fe/src/App.tsx
@@ -1,12 +1,36 @@
 import Header from "./Header";
 import MainBody from "./MainBody";
-
+import { useEffect, useState } from "react";
 function App() {
+  const SERVER_URL = import.meta.env.VITE_SERVER_URL;
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const res = await fetch(`${SERVER_URL}/items`);
+      const data = await res.json();
+      setItems(data);
+    };
+
+    fetchItems();
+  }, []);
+
+  function handleSearch(e: React.FormEvent, keyword: string) {
+    e.preventDefault();
+
+    fetch(`${SERVER_URL}/search/${keyword}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }
   return (
     <>
-      <Header />
+      <Header handleSearch={handleSearch} />
       <main className="bg-dark" style={{ minHeight: "calc(100dvh - 4.5rem)" }}>
-        <MainBody />
+        <MainBody items={items} />
       </main>
     </>
   );
